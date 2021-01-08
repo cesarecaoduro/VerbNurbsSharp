2ae3020d2ed78137cc8bcee1ed31375f519721c1
Cesare Caoduro <Cesare.Caoduro@aecom.com>
2021-01-08 14:15:31 +1100

    minor updates

--- VerbNurbsSharp.Blazor.Babylon/wwwroot/babylonInterop.js
+++ VerbNurbsSharp.Blazor.Babylon/wwwroot/babylonInterop.js
@@ -10,9 +10,11 @@
         objRef[this.objRefKey] = id;
         return objRef;
     },
+
     removeObjectRef: function (id) {
         delete this.objRefs[id];
     },
+
     createEngine : function (canvasId, antialias) {
         var babylonCanvas = document.getElementById(canvasId);
         var babylonEngine = new BABYLON.Engine(babylonCanvas, antialias);
@@ -21,9 +23,11 @@
         });
         return this.storeObjRef(babylonEngine);
     },
+
     createScene : function (engine) {
         return this.storeObjRef(new BABYLON.Scene(engine));
     },
+
     runRenderLoop: function (engine, scene) {
         engine.runRenderLoop(function () {
             scene.render();
